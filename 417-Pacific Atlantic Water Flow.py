04c1d8f0702c681f129ddcd7637adcb54f0dee95
James <james.stemhub@codesmine.com>
2022-08-31 23:11:42 +0800

    feat: 2022/08/31 417

--- 417-Pacific Atlantic Water Flow.py
+++ 417-Pacific Atlantic Water Flow.py
@@ -1,53 +1,41 @@
-# -*- coding: utf-8 -*-
-# @Author  : James Hu
-# @Time    : 31/8/2022 1:12 PM
-from typing import List
-
-
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        if not heights:
-            return []
         m, n = len(heights), len(heights[0])
-        pacific = [[False] * n for _ in range(m)]
-        atlantic = [[False] * n for _ in range(m)]
 
-        def flow(grid, i, j, x, y):
-            if i==x and j==y:
+        atlantic_vis = [[False for _ in range(n)] for _ in range(m)]
+        pacific_vis = [[False for _ in range(n)] for _ in range(m)]
 
-            if i == 0 or j == 0:
-                pacific[x][y] = True
-                if i == m - 1 or j == n - 1:
-                    atlantic[x][y] = True
-                return
-            if i == m - 1 or j == n - 1:
-                atlantic[x][y] = True
-                if i == 0 or j == 0:
-                    pacific[x][y] = True
-                return
-            if grid[i - 1][j] <= grid[i][j]:
-                flow(grid, i - 1, j, x, y)
-            if grid[i + 1][j] <= grid[i][j]:
-                flow(grid, i + 1, j, x, y)
-            if grid[i][j - 1] <= grid[i][j]:
-                flow(grid, i, j - 1, x, y)
-            if grid[i][j + 1] <= grid[i][j]:
-                flow(grid, i, j + 1, x, y)
-            return
+        atlantic = deque()
+        pacific = deque()
 
-        for i in range(m):
-            for j in range(n):
-                flow(heights, i, j,i,j)
-        print(pacific)
-        print(atlantic)
-        res = []
-        for i in range(m):
-            for j in range(n):
-                if pacific[i][j] and atlantic[i][j]:
-                    res.append([i, j])
-        return res
+        for r in range(m):
+            for c in range(n):
+                if r == 0 or c == 0:
+                    pacific_vis[r][c] = True
+                    pacific.append((r, c))
+                if r == m - 1 or c == n - 1:
+                    atlantic_vis[r][c] = True
+                    atlantic.append((r, c))
 
+        def bfs(queue, vis_mat):
+            reach = set()
 
-s = Solution()
-heights = [[1, 2, 2, 3, 5], [3, 2, 3, 4, 4], [2, 4, 5, 3, 1], [6, 7, 1, 4, 5], [5, 1, 1, 2, 4]]
-print(s.pacificAtlantic(heights))
+            while queue:
+                r, c = queue.popleft()
+                reach.add((r, c))
+
+                for nr, nc in [(r, c - 1), (r, c + 1), (r + 1, c), (r - 1, c)]:
+                    if not (0 <= nr < m and 0 <= nc < n): continue
+                    if vis_mat[nr][nc]: continue
+
+                    if heights[nr][nc] >= heights[r][c]:
+                        vis_mat[nr][nc] = True
+                        queue.append((nr, nc))
+            return reach
+
+        # atlantic BFS
+        atlantic_reach = bfs(atlantic, atlantic_vis)
+        # pacific BFS
+        pacific_reach = bfs(pacific, pacific_vis)
+
+        return [[r, c] for r in range(m) for c in range(n) if (r, c) in pacific_reach and (r, c) in atlantic_reach]
