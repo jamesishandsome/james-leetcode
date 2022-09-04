6752e7da08e6ca444b6273e9593394b950047632
James <james.stemhub@codesmine.com>
2022-09-04 21:57:53 +0800

    feat: 2022/09/04 leetcode daily

---
+++ 987-Vertical Order Traversal of a Binary Tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+from collections import defaultdict
+from typing import Optional
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        d = defaultdict(list)
+
+        def func(node, y, x):
+            if not node:
+                return
+            if d[x] == []:
+                d[x] = defaultdict(list)
+            d[x][y].append(node.val)
+            left, right = node.left, node.right
+            func(left, y + 1, x - 1)
+            func(right, y + 1, x + 1)
+
+        func(root, 0, 0)
+        res = []
+        key = list(d.keys())
+        print(d)
+        key.sort()
+        for i in key:
+            key2 = list(d[i].keys())
+            res.append([])
+            key2.sort()
+            for j in key2:
+                d[i][j].sort()
+                res[-1].extend(d[i][j])
+        return res
